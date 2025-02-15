ba9b2d3550a3be95a83b6dc0c6389f2c38277f70
AhmedSaid3617 <said.ahmed3617@gmail.com>
2025-02-15 15:17:09 +0200

    Restructured files.

---
+++ CONFIG/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* ======================================= TRAVEL ======================================= */
+
+#define	LINEAR_POT_PEAK		100
+
+#define  NO_OF_TRAVEL_SENSORS        4
+#define  MAX_VALUE_TRAVEL_SENSOR     4095
+
+#define  NO_OF_PRESSURE_SENSORS      2
+#define  MAX_VALUE_PRESSURE_SENSOR   4095
+
+/* ===================================== END TRAVEL ===================================== */
+
+#endif
